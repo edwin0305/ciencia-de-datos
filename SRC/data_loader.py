9967e49af02e1d02f3313eb2d652dfdb0064abe8
edwin0305 <santiagocantor.2004@gmail.com>
2025-11-21 15:14:51 -0500

    movio algo779799

--- SRC/data_loader.py
+++ SRC/data_loader.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-def cargar_datos():
-    ruta = os.path.join("data", "raw", "diabetes.csv")
-    df = pd.read_csv(ruta)
-    return df
+def load_diabetes_data():
+    base_path = os.path.dirname(os.path.dirname(__file__))
+    file_path = os.path.join(base_path, "data", "diabetes.csv")
+    return pd.read_csv(file_path)
